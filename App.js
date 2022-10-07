0f17182b6cedf909f8b3432553c0b2cfcd9065e2
dfalcon005 <dfalcon005@gmail.com>
2022-10-07 13:37:31 -0500

    !

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to my motherfucking app</Text>
+      <Text>Welcome to my motherfucking app!</Text>
       <StatusBar style="auto" />
     </View>
   );
